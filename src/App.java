96123fcfa63f561a44aee9c91ad5d75dede64edf
Gustavo <126093633+GustavoII2@users.noreply.github.com>
2023-08-02 22:59:53 -0300

    Update App.java
    alteração a mao no github

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Nao entendo , mas entenderei");
+
+        //alterado pelo github
     }
 }
